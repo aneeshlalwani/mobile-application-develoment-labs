767bc2882a280f960c018914c53cccad432fdea5
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-03-12 02:13:26 +0500

    lab-2 added

--- lab-1/task-1/app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ lab-1/task-1/app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 
         val myButton: Button = findViewById(R.id.button)
 
-
         myButton.setOnClickListener {
             // Code to be executed when the button is clicked
 
